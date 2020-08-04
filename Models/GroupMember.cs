c12db861a2e5ab67f3e5385689d0cfa2e0d45863
Spenser Davies <spenser.davies@gmail.com>
2020-08-04 16:40:07 -0600

    GroupMember model updated?

--- Models/GroupMember.cs
+++ Models/GroupMember.cs
@@ -6,5 +6,7 @@ namespace movieparty.Models
         public int GroupId { get; set; }
         public int MemberId { get; set; }
         public string UserId { get; set; }
+        public string Status { get; set; }
+        public bool HasVoted { get; set; }
     }
 }

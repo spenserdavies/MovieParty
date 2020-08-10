395c96444db37a3f3cad17f2997632ad8bbdbdaf
austin <ap.hugus@gmail.com>
2020-08-10 15:01:34 -0600

    updated movie

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -2,13 +2,18 @@ namespace movieparty.Models
 {
     public class Movie
     {
-       public int Id { get; set; }
-       public string Title { get; set; }  
-       public string Img { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ActorOne { get; set; }
+        public string ActorTwo { get; set; }
+        public string ActorThree { get; set; }
+        public string Runtime { get; set; }
+        public string ReleaseDate { get; set; }
+        public string Img { get; set; }
     }
 
     public class GroupMovieViewModel : Movie
     {
-    public int GroupMovieId { get; set; }
+        public int GroupMovieId { get; set; }
     }
 }
